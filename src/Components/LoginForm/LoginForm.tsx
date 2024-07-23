bbd63f4c98949bedc742ae06251e281463d8ba71
kacper-echaust <k9echaust@gmail.com>
2024-07-23 07:54:03 +0200

    rename path

--- src/Components/LoginForm/LoginForm.tsx
+++ src/Components/LoginForm/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent } from 'react'
 import { useAuthorization } from '../../hooks/useAuthorization'
-import { useLogin } from '../..//hooks/useLogin'
+import { useLogin } from '../../hooks/useLogin'
 import css from './LoginForm.module.scss'
 
 const LoginForm = () => {
